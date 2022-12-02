8cf0e6683f59d4d164b5e7a95a12c7f9bb5f9a99
Steve <3835734+stevebanik@users.noreply.github.com>
2022-12-02 12:41:38 -0600

    Create report-disk-usage-by-file-type.sh

---
+++ report-disk-usage-by-file-type.sh
@@ -0,0 +1,1 @@
+find . -type f -empty -prune -o -type f -printf "%s\t" -exec file --brief --mime-type '{}' \; | awk 'BEGIN {printf("%12s\t%12s\n","bytes","type")} {type=$2; a[type]+=$1} END {for (i in a) printf("%12u\t%12s\n", a[i], i)|"sort -nr"}'
